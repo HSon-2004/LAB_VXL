c79ddf78dd36291f39b7a154d7f21c59364a5d21
HSon <haison120404@gmail.com>
2024-12-17 18:39:01 +0700

    add lab4

---
+++ LAB4/Core/Inc/fsm_setting.h
@@ -0,0 +1,13 @@
+/*
+ * fsm_setting.h
+ *
+ *  Created on: Nov 11, 2024
+ *      Author: Son
+ */
+
+#ifndef INC_FSM_SETTING_H_
+#define INC_FSM_SETTING_H_
+
+void fsm_setting_run();
+
+#endif /* INC_FSM_SETTING_H_ */
---
+++ LAB4/Core/Src/global.c
@@ -0,0 +1,11 @@
+/*
+ * global.c
+ *
+ *  Created on: Nov 11, 2024
+ *      Author: Son
+ */
+
+#include "global.h"
+
+int status[2] = {0,0};
+int timerLed[3] = {5000,3000,2000};
